91f91047314e708a87422edd855a5a59943c2fe2
a <fred.shepherd@music-group.com>
2018-09-26 17:22:18 +0100

    check 1 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import styled from 'styled-components'
+
+import Loader from './Loader'
+
+const Box = styled.div`
+  height: 500px;
+  width: 400px;
+  position: relative;
+  background: grey;
+`
+
 const App = _ => (
-  <div>yolo</div>
+  <Box>
+    <Loader />
+  </Box>
 )
 
 ReactDOM.render(
